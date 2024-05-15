978a649039699e03a2c1ac3058c7f24a8bc4c8e6
Joji george <140682832+jojojigeorge@users.noreply.github.com>
2024-05-15 17:23:03 +0530

    Update index.js url

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,13 @@ const app=express()
 // middleware
 app.use(express.json())
 app.use(morgan('dev'))
-app.use(cors())
+app.use(cors(
+  {
+    origin:["https://parking-app-001-client.vercel.app"],
+    method:["POST","GET","PUT","DELETE"],
+    credentials:true
+  }
+))
 
 // routes
 app.use('/api/v1/user',UserRouter)
