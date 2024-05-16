5d7a0da3c33441140233b3cffb7499cfccc5498f
Joji george <140682832+jojojigeorge@users.noreply.github.com>
2024-05-16 15:29:19 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -19,11 +19,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "https://parking-app-backend-omega.vercel.app/",
-        changeOrigin: true,
-        secure: false,
       },
     },
-
-    
   },
 })
